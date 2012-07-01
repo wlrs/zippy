a6b21326b1ab350b726b1f10e398934ff0923b76
Joey Nelson <joey@wlrs.net>
2012-07-01 00:20:45 -0400

    dump into db

--- script/import.rb
+++ script/import.rb
@@ -1,7 +1,10 @@
 require 'csv'
 require 'pp'
+require 'mysql'
 
 columns = nil
+db = Mysql.real_connect('localhost', 'root', '', 'zippy')
+
 
 CSV.foreach("../source/free-zipcode-database.csv") do |row|
   if !columns
@@ -11,7 +14,12 @@ CSV.foreach("../source/free-zipcode-database.csv") do |row|
 
   zip = {}
 
-  columns.each_with_index { |col, index| zip[col] = row[index] }
-  pp zip
+  columns.each_with_index do |col, index|
+    zip[col.downcase] = db.quote(row[index]) if row[index]
+  end
+
+  puts "#{zip['zipcode']} - #{zip['city']}, #{zip['state']}"
+
+  db.query("insert into `zips` (`zip`, `city`, `state`, `updated`) VALUES ('#{zip['zipcode']}', '#{zip['city']}', '#{zip['state']}', now())")
 
 end
